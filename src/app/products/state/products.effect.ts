1112f0d457bff537ce2b8cba30010f46ac50867b
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-12-19 13:00:40 -0300

    Products, add update delete: add effects and reducer.

--- src/app/products/state/products.effect.ts
+++ src/app/products/state/products.effect.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { ProductsService } from "../products.service";
 import { ProductsAPIActions, ProductsPageActions } from "./products.action";
-import { catchError, concatMap, map, of } from "rxjs";
+import { catchError, concatMap, exhaustMap, map, mergeMap, of } from "rxjs";
 
 @Injectable()
 export class ProductEffects {
@@ -11,13 +11,46 @@ export class ProductEffects {
   loadProducts$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ProductsPageActions.loadProducts),
-      concatMap(() => this.productsService
-        .getAll()
-        .pipe(
-          map((products) =>
-            ProductsAPIActions.productsLoadedSuccess({ products })
-          ),
+      exhaustMap(() =>
+        this.productsService.getAll().pipe(
+          map((products) => ProductsAPIActions.productsLoadedSuccess({ products })),
           catchError((error) => of(ProductsAPIActions.productsLoadedFail({ message: error })))
+        )
+      )
+    )
+  );
+
+  addProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.addProduct),
+      mergeMap(({ product }) =>
+        this.productsService.add(product).pipe(
+          map((newProduct) => ProductsAPIActions.productsAddedSuccess({ product: newProduct })),
+          catchError((error) => of(ProductsAPIActions.productsAddedFail({ message: error })))
+        )
+      )
+    )
+  );
+
+  updateProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.updateProduct),
+      concatMap(({ product }) =>
+        this.productsService.update(product).pipe(
+          map(() => ProductsAPIActions.productsUpdatedSuccess({ product })),
+          catchError((error) => of(ProductsAPIActions.productsUpdatedFail({ message: error })))
+        )
+      )
+    )
+  );
+
+  deleteProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.deleteProduct),
+      mergeMap(({ id }) =>
+        this.productsService.delete(id).pipe(
+          map(() => ProductsAPIActions.productsDeletedSuccess({ id })),
+          catchError((error) => of(ProductsAPIActions.productsDeletedFail({ message: error })))
         )
       )
     )
--- src/app/products/state/products.reducer.ts
+++ src/app/products/state/products.reducer.ts
@@ -18,10 +18,12 @@ const initialState: ProductsState = {
 
 export const productsReducer = createReducer(
   initialState,
+
   on(ProductsPageActions.toggleShowProductCode, (state) => ({
     ...state,
     showProductCode: !state.showProductCode
   })),
+
   on(ProductsPageActions.loadProducts, (state) => ({
     ...state,
     loading: true,
@@ -38,5 +40,53 @@ export const productsReducer = createReducer(
     products: [],
     errorMessage: message,
     loading: false
+  })),
+
+  on(ProductsPageActions.addProduct, (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: ''
+  })),
+  on(ProductsAPIActions.productsAddedSuccess, (state, { product }) => ({
+    ...state,
+    loading: false,
+    products: [...state.products, product] //we must never mutate the returned state. Using push here to push the new product onto the array, essentially mutating it, will not work and break the principle of immutability. You should always return all new state slices.
+  })),
+  on(ProductsAPIActions.productsAddedFail, (state, { message }) => ({
+    ...state,
+    loading: false,
+    errorMessage: message
+  })),
+
+  on(ProductsPageActions.updateProduct, (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: ''
+  })),
+  on(ProductsAPIActions.productsUpdatedSuccess, (state, { product }) => ({
+    ...state,
+    loading: false,
+    products: state.products.map(existingProduct => existingProduct.id === product.id ? product : existingProduct) //we're using the non‑mutation map array method to make sure we return a new slice of state.
+  })),
+  on(ProductsAPIActions.productsUpdatedFail, (state, { message }) => ({
+    ...state,
+    loading: false,
+    errorMessage: message
+  })),
+
+  on(ProductsPageActions.deleteProduct, (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: ''
+  })),
+  on(ProductsAPIActions.productsDeletedSuccess, (state, { id }) => ({
+    ...state,
+    loading: false,
+    products: state.products.filter(existingProduct => existingProduct.id !== id) //we're using the non‑mutation filter array method to make sure we return a new slice of state.
+  })),
+  on(ProductsAPIActions.productsDeletedFail, (state, { message }) => ({
+    ...state,
+    loading: false,
+    errorMessage: message
   }))
 )
