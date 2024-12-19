f1bed42714c455863c8d63be63c1a3d2a0523fd3
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-12-19 16:39:22 -0300

    Product page: Router store selectors to get one product from the store.

--- src/app/products/state/products.selector.ts
+++ src/app/products/state/products.selector.ts
@@ -1,6 +1,7 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { ProductsState } from "./products.reducer";
 import { sumProducts } from "src/app/utils/sum-products";
+import { getRouterSelectors } from "@ngrx/router-store";
 
 const selectProductsState = createFeatureSelector<ProductsState>('products');
 
@@ -29,4 +30,13 @@ export const selectProductsTotal = createSelector(
   //the signature of the projector function is the same as the sumProducts. So we can just pass the function only.
   // (products) => sumProducts(products)
   sumProducts
+);
+
+const { selectRouteParams } = getRouterSelectors(); //we are destructuring a collection of route selectors from NgRx by calling getRouterSelectors.
+
+export const selectProductById = createSelector(
+  selectProducts,
+  selectRouteParams,
+  (products, { id }) => products.find(product => product.id === parseInt(id))
+  //we are destructuring the id property from selectRouteParams.
 );
