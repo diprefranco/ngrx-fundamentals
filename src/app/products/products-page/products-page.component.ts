dc3dd398bad732499e36db3cc23d5aa5161457e9
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-12-20 11:25:28 -0300

    Products: @ngrx/component-store library applied for the products store instead of @ngrx/store.

--- src/app/products/products-page/products-page.component.ts
+++ src/app/products/products-page/products-page.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ProductsPageActions } from '../state/products.action';
 import { selectProducts, selectProductsErrorMessage, selectProductsLoading, selectProductsShowProductCode, selectProductsTotal } from '../state/products.selector';
+import { ProductsStore } from '../products.store';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  providers: [ProductsStore]
 })
-export class ProductsPageComponent {
-  products$ = this.store.select(selectProducts);
+export class ProductsPageComponent implements OnInit {
+  products$ = this.productsStore.products$;
+  // products$ = this.store.select(selectProducts);
   total$ = this.store.select(selectProductsTotal);
   loading$ = this.store.select(selectProductsLoading);
   showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage$ = this.store.select(selectProductsErrorMessage);
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private productsStore: ProductsStore) {
     this.store.subscribe(store => console.log(store));
+  }
+
+  ngOnInit() {
+    this.productsStore.getProducts();
   }
 
   toggleShowProductCode() {
